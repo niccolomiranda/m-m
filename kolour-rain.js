e8707528e48a2e513751b219f5986feebf7a5f1c
Niccolò Miranda <vianiccolo@gmail.com>
2019-10-01 19:43:06 +0200

    Update kolour-rain.js

--- kolour-rain.js
+++ kolour-rain.js
@@ -1,259 +1,268 @@
+
+
 const PARAMS = {
-    AMOUNT: 100,
-    MAX_AMOUNT: 1000,
-    SCALE_BASE: 0.5,
-    SPEED_BASE: 1,
-    ROTATIONSPEED_BASE: 0.03,
-    RADIUS: 100,
-    STRENGTH: 3
+  AMOUNT: 100,
+  MAX_AMOUNT: 2000,
+  SCALE_BASE: 0.3,
+  SPEED_BASE: 1,
+  ROTATIONSPEED_BASE: 0.03,
+  RADIUS: 100,
+  STRENGTH: 3
 };
 
-const gui = new dat.GUI();
+// const gui = new dat.GUI();
 
-gui
-    .add(PARAMS, "RADIUS")
-    .min(50)
-    .max(200);
+// gui
+//   .add(PARAMS, "RADIUS")
+//   .min(50)
+//   .max(200);
 
-gui
-    .add(PARAMS, "STRENGTH")
-    .min(1)
-    .max(5);
+// gui
+//   .add(PARAMS, "STRENGTH")
+//   .min(1)
+//   .max(5);
 
-gui
-    .add(PARAMS, "MAX_AMOUNT")
-    .min(500)
-    .max(10000);
+// gui
+//   .add(PARAMS, "MAX_AMOUNT")
+//   .min(500)
+//   .max(10000);
 
-class App extends PIXI.Application {
-    constructor(parameters) {
-        super(parameters);
-    }
+class AppRain extends PIXI.Application {
+  constructor(parameters) {
+    super(parameters);
+  }
 
-    loadTextures() {
-        return new Promise((resolve, reject) => {
-            if (!this.textures) {
-                assetsToTextures(assets).then(textures => {
-                    this.textures = textures;
-                    resolve();
-                });
-            } else {
-                resolve();
-            }
+  loadTextures() {
+    return new Promise((resolve, reject) => {
+      if (!this.textures) {
+        assetsToTextures(assets).then(textures => {
+          this.textures = textures;
+          resolve();
         });
-    }
+      } else {
+        resolve();
+      }
+    });
+  }
 
-    startRainingEffect() {
-        this.loadTextures().then(this.createAssets.bind(this));
-    }
+  startRainingEffect() {
+    this.loadTextures().then(this.createAssets.bind(this));
+  }
 
-    init() {
-        this.mouse = vec2.fromValues(-1000, -1000);
-        window.addEventListener("touchstart", this.onTouchStart.bind(this), false);
-        window.addEventListener("touchmove", this.onTouchMove.bind(this), false);
-        window.addEventListener("touchend", this.onTouchEnd.bind(this), false);
-        window.addEventListener("mousemove", this.onMouseMove.bind(this), false);
-        window.addEventListener("mousedown", this.onMouseDown.bind(this), false);
-        window.addEventListener("mouseup", this.onMouseUp.bind(this), false);
-        document.addEventListener(
-            "contextmenu",
-            event => event.preventDefault(),
-            false
+  init() {
+    this.mouse = vec2.fromValues(-1000, -1000);
+    window.addEventListener("touchstart", this.onTouchStart.bind(this), false);
+    window.addEventListener("touchmove", this.onTouchMove.bind(this), false);
+    window.addEventListener("touchend", this.onTouchEnd.bind(this), false);
+    window.addEventListener("mousemove", this.onMouseMove.bind(this), false);
+    window.addEventListener("mousedown", this.onMouseDown.bind(this), false);
+    window.addEventListener("mouseup", this.onMouseUp.bind(this), false);
+    // document.addEventListener(
+    //   "contextmenu",
+    //   event => event.preventDefault(),
+    //   false
+    // );
+    let canvasElement = document.createElement("div");
+    canvasElement.id = "canvas";
+    document.body.appendChild(canvasElement);
+    canvasElement.appendChild(this.view);
+    this.resizeTo = canvasElement;
+    this.time = 0;
+    this.ticker.add(delta => {
+      this.time += 1;
+      if (this.mouseDown && Math.round(this.time % 6) === 1) {
+        let randomTexture = this.textures[
+          Math.floor(Math.random() * this.textures.length)
+        ];
+        let position = vec2.create();
+        vec2.add(
+          position,
+          this.mouseDown,
+          vec2.fromValues(
+            Math.random() * 100 * (Math.random() > 0.5 ? 1 : -1),
+            Math.random() * 100 * (Math.random() > 0.5 ? 1 : -1)
+          )
         );
-        let canvasElement = document.createElement("div");
-        canvasElement.id = "canvas";
-        document.body.appendChild(canvasElement);
-        canvasElement.appendChild(this.view);
-        this.resizeTo = canvasElement;
-        this.ticker.add(delta => {
-            if (this.mouseDown) {
-                let randomTexture = this.textures[
-                    Math.floor(Math.random() * this.textures.length)
-                ];
-                let position = vec2.create();
-                vec2.add(
-                    position,
-                    this.mouseDown,
-                    vec2.fromValues(
-                        Math.random() * 100 * (Math.random() > 0.5 ? 1 : -1),
-                        Math.random() * 100 * (Math.random() > 0.5 ? 1 : -1)
-                    )
-                );
-                this.assets.push(new Asset(randomTexture, position));
-                if (this.assets.length > PARAMS.MAX_AMOUNT) {
-                    this.assets.shift().remove();
-                }
-            }
-            if (this.assets) {
-                this.assets.forEach(asset => {
-                    asset.update();
-                });
-            }
+        this.assets.push(new Asset(randomTexture, position));
+        if (this.assets.length > PARAMS.MAX_AMOUNT) {
+          this.assets.shift().remove();
+        }
+      }
+      if (this.assets) {
+        this.assets.forEach(asset => {
+          asset.update();
         });
+      }
+    });
 
-        return this;
-    }
+    return this;
+  }
 
-    createAssets() {
-        this.assets = [];
-        for (let i = 0; i < PARAMS.AMOUNT; i++) {
-            let randomTexture = this.textures[
-                Math.floor(Math.random() * this.textures.length)
-            ];
-            this.assets.push(new Asset(randomTexture));
-        }
-        gui
-            .add(this.assets, "length")
-            .listen()
-            .name("assets amount");
+  createAssets() {
+    this.assets = [];
+    for (let i = 0; i < PARAMS.AMOUNT; i++) {
+      let randomTexture = this.textures[
+        Math.floor(Math.random() * this.textures.length)
+      ];
+      this.assets.push(new Asset(randomTexture));
     }
+    // gui
+    //   .add(this.assets, "length")
+    //   .listen()
+    //   .name("assets amount");
+  }
 
-    onMouseMove(e) {
-        // this.mouseVelocity = vec2.fromValues(
-        //   1 + Math.abs(e.movementX) / 5,
-        //   1 + Math.abs(e.movementY) / 5
-        // );
-        this.mouse = vec2.fromValues(e.clientX, e.clientY);
-        if (this.mouseDown) this.mouseDown = this.mouse;
-    }
+  onMouseMove(e) {
+    // this.mouseVelocity = vec2.fromValues(
+    //   1 + Math.abs(e.movementX) / 5,
+    //   1 + Math.abs(e.movementY) / 5
+    // );
+    this.mouse = vec2.fromValues(e.clientX, e.clientY);
+    if (this.mouseDown) this.mouseDown = this.mouse;
+  }
 
-    onMouseDown(e) {
-        this.mouseDown = vec2.fromValues(e.clientX, e.clientY);
-    }
+  onMouseDown(e) {
+    this.mouseDown = vec2.fromValues(e.clientX, e.clientY);
+  }
 
-    onMouseUp(e) {
-        this.mouseDown = null;
-    }
+  onMouseUp(e) {
+    this.mouseDown = null;
+  }
 
-    onTouchStart(e) {
-        if (e.touches[1]) {
-            this.mouseDown = vec2.fromValues(
-                e.touches[0].clientX,
-                e.touches[0].clientY
-            );
-        }
-    }
+  onTouchStart(e) {
+    //if (e.touches[1]) {
+    // this.mouseDown = vec2.fromValues(
+    //   e.touches[0].clientX,
+    //   e.touches[0].clientY
+    // );
+    //}
+  }
 
-    onTouchMove(e) {
-        this.mouse = vec2.fromValues(e.touches[0].clientX, e.touches[0].clientY);
-        if (this.mouseDown) this.mouseDown = this.mouse;
-    }
+  onTouchMove(e) {
+    this.mouse = vec2.fromValues(e.touches[0].clientX, e.touches[0].clientY);
+    if (this.mouseDown) this.mouseDown = this.mouse;
+  }
 
-    onTouchEnd(e) {
-        this.mouse = vec2.fromValues(-1000, -1000);
-        this.mouseDown = null;
-    }
+  onTouchEnd(e) {
+    this.mouse = vec2.fromValues(-1000, -1000);
+    this.mouseDown = null;
+  }
 }
 
 class Asset {
-    constructor(texture, position = null) {
-        this.position = position;
-        this.texture = texture;
-        this.init();
+  constructor(texture, position = null) {
+    this.position = position;
+    this.texture = texture;
+    this.init();
+  }
+
+  init() {
+    this.sprite = new PIXI.Sprite(this.texture);
+
+    this.scale = PARAMS.SCALE_BASE + Math.random() / 10;
+
+    this.size = vec2.fromValues(
+      this.texture.baseTexture.width * this.scale,
+      this.texture.baseTexture.height * this.scale
+    );
+
+    this.position =
+      this.position ||
+      vec2.fromValues(
+        appRain.screen.width * Math.random(),
+        -(appRain.screen.height + this.size[1] * 10) * Math.random() -
+          this.size[1] / 2
+      );
+    this.speed = vec2.fromValues(0, PARAMS.SPEED_BASE + Math.random() / 2);
+    this.rotation = Math.random() * 360;
+    this.rotationSpeed = PARAMS.ROTATIONSPEED_BASE + Math.random() / 100;
+
+    this.sprite.anchor.x = 0.5;
+    this.sprite.anchor.y = 0.5;
+
+    appRain.stage.addChild(this.sprite);
+  }
+
+  update() {
+    // this.size = vec2.fromValues(
+    //   this.texture.baseTexture.width,
+    //   this.texture.baseTexture.height
+    // );
+
+    vec2.add(this.position, this.position, this.speed);
+
+    if (vec2.distance(this.position, appRain.mouse) < PARAMS.RADIUS) {
+      let v = vec2.create();
+      vec2.subtract(v, this.position, appRain.mouse);
+      // // let v = vec2.fromValues(0, 0);
+      vec2.normalize(v, v);
+      // // vec2.multiply(v,v,appRain.mouseVelocity)
+      vec2.multiply(v, v, vec2.fromValues(PARAMS.STRENGTH, PARAMS.STRENGTH));
+      vec2.add(this.position, this.position, v);
+      this.rotation += this.rotationSpeed;
     }
 
-    init() {
-        this.sprite = new PIXI.Sprite(this.texture);
-
-        this.scale = PARAMS.SCALE_BASE + Math.random() / 10;
-
-        this.size = vec2.fromValues(
-            this.texture.baseTexture.width * this.scale,
-            this.texture.baseTexture.height * this.scale
-        );
-
-        this.position =
-            this.position ||
-            vec2.fromValues(
-                app.screen.width * Math.random(),
-                -(app.screen.height + this.size[1] * 10) * Math.random() -
-                this.size[1] / 2
-            );
-        this.speed = vec2.fromValues(0, PARAMS.SPEED_BASE + Math.random() / 2);
-        this.rotation = Math.random() * 360;
-        this.rotationSpeed = PARAMS.ROTATIONSPEED_BASE + Math.random() / 100;
-
-        this.sprite.anchor.x = 0.5;
-        this.sprite.anchor.y = 0.5;
-
-        app.stage.addChild(this.sprite);
+    if (this.position[1] - this.size[1] > appRain.screen.height) {
+      this.position = vec2.fromValues(
+        appRain.screen.width * Math.random(),
+        -(appRain.screen.height + this.size[1] * 10) * Math.random() -
+          this.size[1] / 2
+      );
     }
+    this.sprite.x = this.position[0];
+    this.sprite.y = this.position[1];
 
-    update() {
-        // this.size = vec2.fromValues(
-        //   this.texture.baseTexture.width,
-        //   this.texture.baseTexture.height
-        // );
+    this.sprite.scale.x = this.scale;
+    this.sprite.scale.y = this.scale;
 
-        vec2.add(this.position, this.position, this.speed);
+    this.sprite.rotation = this.rotation;
+  }
 
-        if (vec2.distance(this.position, app.mouse) < PARAMS.RADIUS) {
-            let v = vec2.create();
-            vec2.subtract(v, this.position, app.mouse);
-            // // let v = vec2.fromValues(0, 0);
-            vec2.normalize(v, v);
-            // // vec2.multiply(v,v,app.mouseVelocity)
-            vec2.multiply(v, v, vec2.fromValues(PARAMS.STRENGTH, PARAMS.STRENGTH));
-            vec2.add(this.position, this.position, v);
-            this.rotation += this.rotationSpeed;
-        }
-
-        if (this.position[1] - this.size[1] > app.screen.height) {
-            this.position = vec2.fromValues(
-                app.screen.width * Math.random(),
-                -(app.screen.height + this.size[1] * 10) * Math.random() -
-                this.size[1] / 2
-            );
-        }
-        this.sprite.x = this.position[0];
-        this.sprite.y = this.position[1];
-
-        this.sprite.scale.x = this.scale;
-        this.sprite.scale.y = this.scale;
-
-        this.sprite.rotation = this.rotation;
-    }
-
-    remove() {
-        app.stage.removeChild(this.sprite);
-    }
+  remove() {
+    appRain.stage.removeChild(this.sprite);
+  }
 }
 
 const assets = [
-    "https://uploads-ssl.webflow.com/5b67e2c8dc6fa8e26712d6d5/5d933012e2f2d30f9a11b367_ass3.svg",
-    "https://uploads-ssl.webflow.com/5b67e2c8dc6fa8e26712d6d5/5d9358021d15370ab2b3e99c_ass11.svg",
-    "https://uploads-ssl.webflow.com/5b67e2c8dc6fa8e26712d6d5/5d932c7d67afa8599ad186fa_ass5.svg",
-    "https://uploads-ssl.webflow.com/5b67e2c8dc6fa8e26712d6d5/5d932c78e2f2d377b0117e0c_ass2.svg"
-
+  "https://rocheclement.fr/public/images/kolour/1.svg",
+  "https://rocheclement.fr/public/images/kolour/2.svg",
+  "https://rocheclement.fr/public/images/kolour/3.svg",
+  "https://rocheclement.fr/public/images/kolour/4.svg",
+  "https://rocheclement.fr/public/images/kolour/5.svg",
+  "https://rocheclement.fr/public/images/kolour/6.svg",
+  "https://rocheclement.fr/public/images/kolour/7.svg",
+  "https://rocheclement.fr/public/images/kolour/8.svg",
+  "https://rocheclement.fr/public/images/kolour/9.svg",
+  "https://rocheclement.fr/public/images/kolour/10.svg"
 ];
 
 function assetsToTextures(assets) {
-    const loader = new PIXI.Loader();
-    const textures = [];
-    assets.forEach((asset, index) => {
-        loader.add(index.toString(), asset);
+  const loader = new PIXI.Loader();
+  const textures = [];
+  assets.forEach((asset, index) => {
+    loader.add(index.toString(), asset);
+  });
+  loader.load((loader, resources) => {});
+  return new Promise((resolve, reject) => {
+    loader.onComplete.add((loader, resources) => {
+      Object.keys(resources).forEach(key => {
+        let resource = resources[key];
+        textures.push(resource.texture);
+        resolve(textures);
+      });
     });
-    loader.load((loader, resources) => {});
-    return new Promise((resolve, reject) => {
-        loader.onComplete.add((loader, resources) => {
-            Object.keys(resources).forEach(key => {
-                let resource = resources[key];
-                textures.push(resource.texture);
-                resolve(textures);
-            });
-        });
-    });
+  });
 }
 
-let app = new App({
-    width: window.innerWidth,
-    height: window.innerHeight,
-    transparent: true,
-    resolution: window.devicePixelRatio || 1,
-    resizeTo: window,
-    // antialias: true,
-    forceFXAA: true,
-    powerPreference: "high-performance"
+let appRain = new AppRain({
+  width: window.innerWidth,
+  height: window.innerHeight,
+  transparent: true,
+  resolution: window.devicePixelRatio || 1,
+  resizeTo: window,
+  // antialias: true,
+  forceFXAA: true,
+  powerPreference: "high-performance"
 });
 
-app.init().startRainingEffect();
+appRain.init().startRainingEffect();
